1547e20d90e3e107501c285f25770ed3c3e7a3fb
Teyoung-Hong <teyon2323@gmail.com>
2019-03-06 20:56:19 +0900

    placeholder

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -5,14 +5,13 @@ from django.conf import settings
 class ContactForm(forms.Form):
     
     name = forms.CharField(label='お名前', required=True, widget=forms.TextInput(attrs={'class': 'form-control reset-border-radius',}))
-    email = forms.EmailField(label='メールアドレス', required=True, widget=forms.TextInput(attrs={'class': 'form-control reset-border-radius',}))
-    message = forms.CharField(label='お問い合わせ内容', widget=forms.Textarea(attrs={'class': 'form-control reset-border-radius',}), required=True)
+    message = forms.CharField(label='お問い合わせ内容', widget=forms.Textarea(attrs={'placeholder':'メールアドレスも併せてご記入お願い致します。', 'class': 'form-control reset-border-radius',}), required=True)
     
     # メール送信処理
     def send_email(self):
         # send email using the self.cleaned_data dictionary
         subject = self.cleaned_data['name']
-        message = self.cleaned_data['email']
+        message = self.cleaned_data['message']
         from_email = settings.EMAIL_HOST_USER
         to = [settings.EMAIL_HOST_USER]
 
